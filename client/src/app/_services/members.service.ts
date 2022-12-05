3e39cd043612116cbdc35143b2cc48920064ce89
YehanYeshminda <81465670+YehanYeshminda@users.noreply.github.com>
2022-12-05 08:07:03 +0530

    restored the caching for members

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -19,10 +19,17 @@ const httpOptions = {
 export class MembersService {
   baseUrl = environment.apiUrl;
   members: Member[] = [];
+  memberCache = new Map(); // used to store the users which we get from the user request
 
   constructor(private http: HttpClient) {}
 
   getMembers(userParams: UserParams) {
+
+    // getting the response and storing inside of the memberCache
+    const response = this.memberCache.get(Object.values(userParams).join('-'));
+
+    if (response) return of(response);
+
     let params = this.getPaginationHeaders(
       userParams.pageNumber,
       userParams.pageSize
@@ -33,7 +40,12 @@ export class MembersService {
     params = params.append('gender', userParams.gender);
     params = params.append('orderBy', userParams.orderBy);
 
-    return this.getPaginatedResult<Member[]>(this.baseUrl + 'users', params);
+    return this.getPaginatedResult<Member[]>(this.baseUrl + 'users', params).pipe(
+      map(response => {
+        this.memberCache.set(Object.values(userParams).join('-'), response);
+        return response;
+      })
+    );
   }
 
   private getPaginatedResult<T>(url: string, params: HttpParams) {
