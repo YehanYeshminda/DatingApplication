a9b572e7438182cb72dec8b186d6471ffaf0b72d
YehanYeshminda <81465670+YehanYeshminda@users.noreply.github.com>
2022-12-06 07:17:13 +0530

    added the liking user functionality

--- client/src/app/lists/lists.component.ts
+++ client/src/app/lists/lists.component.ts
@@ -1,15 +1,25 @@
+import { MembersService } from './../_services/members.service';
+import { Member } from './../_models/Member';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-lists',
   templateUrl: './lists.component.html',
-  styleUrls: ['./lists.component.css']
+  styleUrls: ['./lists.component.css'],
 })
 export class ListsComponent implements OnInit {
+  members: Member[] | undefined;
+  predicate = 'liked';
 
-  constructor() { }
+  constructor(private memberService: MembersService) {}
 
   ngOnInit(): void {
+    this.loadLikes();
   }
 
+  loadLikes() {
+    this.memberService.getLikes(this.predicate).subscribe({
+      next: (res) => (this.members = res),
+    });
+  }
 }
