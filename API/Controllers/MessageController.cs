bc48a7224cd6b250f89e565c5efaaad094879299
YehanYeshminda <81465670+YehanYeshminda@users.noreply.github.com>
2022-12-13 07:55:43 +0530

    added the messaging end points

--- API/Controllers/MessageController.cs
+++ API/Controllers/MessageController.cs
@@ -1,6 +1,7 @@
 using API.DTOs;
 using API.Entities;
 using API.Extensions;
+using API.Helpers;
 using API.Interfaces;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
@@ -47,5 +48,28 @@ namespace API.Controllers
 
             return BadRequest("Failed to send message!");
         }
+
+        [HttpGet]
+        public async Task<ActionResult<PagedList<MessageDto>>> GetMessageFromUser([FromQuery]MessageParams messageParams)
+        {
+            messageParams.Username = User.GetUsername();
+
+            var messages = await _messageRepository.GetMessagesForUser(messageParams);
+
+            Response.AddPaginationHeader(new PaginationHeader(messages.CurrentPage,
+                                                              messages.PageSize,
+                                                              messages.TotalCount,
+                                                              messages.TotalPages));
+
+            return messages;
+        }
+
+        [HttpGet("thread/{username}")]
+        public async Task<ActionResult<IEnumerable<MessageDto>>> GetMessageThread(string username)
+        {
+            var currentUsername = User.GetUsername();
+            return Ok(await _messageRepository.GetMessageThread(currentUsername, username));
+        }
+
     }
 }
