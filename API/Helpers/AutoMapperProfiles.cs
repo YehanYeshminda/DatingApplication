16d0cdc8a4fbaa8e313639be6f4f43ea2b759cbf
YehanYeshminda <81465670+YehanYeshminda@users.noreply.github.com>
2022-12-13 06:17:59 +0530

    added auto mapper profile for message to photos

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -23,6 +23,11 @@ namespace API.Helpers
           CreateMap<Photo, PhotoDto>();
           CreateMap<MemberUpdateDto, AppUser>(); // mapping the member dto to the appuser properties
           CreateMap<RegisterDto, AppUser>(); // mapping the register dto to the app user
+          
+          CreateMap<Message, MessageDto>().ForMember(dest => dest.SendPhotoUrl,
+              opt => opt.MapFrom(s => s.Sender.Photos.FirstOrDefault(x => x.IsMain).Url))
+              .ForMember(dest => dest.RecipientPhotoUrl,
+              opt => opt.MapFrom(s => s.Recipient.Photos.FirstOrDefault(x => x.IsMain).Url));
         }
 
 
