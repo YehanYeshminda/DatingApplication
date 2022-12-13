974e8f68da6f6baaac81aef85fb9a50bc35fbb46
YehanYeshminda <81465670+YehanYeshminda@users.noreply.github.com>
2022-12-13 05:41:19 +0530

    added pagination for the likes in client

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -115,8 +115,13 @@ export class MembersService {
     return this.http.post(this.baseUrl + 'likes/' + username, {});
   }
 
-  getLikes(predicate: string) {
-    return this.http.get<Member[]>(this.baseUrl + 'likes?predicate=' + predicate);
+  // used to get likes in the client sizes
+  getLikes(predicate: string, pageNumber: number, pageSize: number) {
+    let params = this.getPaginationHeaders(pageNumber, pageSize);
+
+    params = params.append("predicate", predicate);
+
+    return this.getPaginatedResult<Member[]>(this.baseUrl + 'likes', params);
   }
 
   private getPaginatedResult<T>(url: string, params: HttpParams) {
--- client/src/app/lists/lists.component.ts
+++ client/src/app/lists/lists.component.ts
@@ -1,3 +1,4 @@
+import { Pagination } from './../_models/Pagination';
 import { MembersService } from './../_services/members.service';
 import { Member } from './../_models/Member';
 import { Component, OnInit } from '@angular/core';
@@ -10,6 +11,9 @@ import { Component, OnInit } from '@angular/core';
 export class ListsComponent implements OnInit {
   members: Member[] | undefined;
   predicate = 'liked';
+  pageNumber = 1;
+  pageSize = 5;
+  pagination: Pagination | undefined;
 
   constructor(private memberService: MembersService) {}
 
@@ -18,8 +22,18 @@ export class ListsComponent implements OnInit {
   }
 
   loadLikes() {
-    this.memberService.getLikes(this.predicate).subscribe({
-      next: (res) => (this.members = res),
+    this.memberService.getLikes(this.predicate, this.pageNumber, this.pageSize).subscribe({
+      next: (res) => {
+        this.members = res.result;
+        this.pagination = res.pagination;
+      },
     });
+  }
+
+  pageChanged(event: any) {
+    if (this.pageNumber !== event.page) {
+      this.pageNumber = event.page;
+      this.loadLikes();
+    }
   }
 }
