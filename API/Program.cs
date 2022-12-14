7dad2d0af31d567e23678ed0c212c0ca5c7a4560
YehanYeshminda <81465670+YehanYeshminda@users.noreply.github.com>
2022-12-14 22:49:06 +0530

    added role management into the application

--- API/Program.cs
+++ API/Program.cs
@@ -18,8 +18,9 @@ namespace API
             {
                 var context = services.GetRequiredService<DataContext>();
                 var userManager = services.GetRequiredService<UserManager<AppUser>>();
+                var roleManager = services.GetRequiredService<RoleManager<AppRole>>();
                 await context.Database.MigrateAsync(); // will create the migrations
-                await Seed.SeedUsers(userManager); // this is used to seed the data
+                await Seed.SeedUsers(userManager, roleManager); // this is used to seed the data
             }
             catch (Exception ex)
             {
