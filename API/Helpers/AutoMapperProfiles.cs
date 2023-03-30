cf6e59d32fe23ae99782e7f7f6dbdefaba8984b2
Yehan Yeshminda <yeshanyesh12@gmail.com>
2023-03-30 07:04:35 +0400

    extended appuse and made the getting of users more advanced and efficient

---
+++ API/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,20 @@
+using API.DTOs;
+using API.Entities;
+using API.Extensions;
+using AutoMapper;
+
+namespace API.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            // if is main true then we get the URL
+            CreateMap<AppUser, MemberDto>()
+                .ForMember(dest => dest.PhotoUrl, opt => opt.MapFrom(s => s.Photos.FirstOrDefault(x => x.IsMain).Url))
+                .ForMember(dest => dest.Age, opt => opt.MapFrom(s => s.DateOfBirth.CalculateAge()));
+
+            CreateMap<Photo, PhotoDto>();
+        }
+    }
+}
